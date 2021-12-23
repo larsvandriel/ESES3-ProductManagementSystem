e354df41ad151f7a6f51d27e49961cffd80d665c
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 20:38:25 +0100

    Update RepositoryContext.cs

--- pms_entities/RepositoryContext.cs
+++ pms_entities/RepositoryContext.cs
@@ -1,7 +1,31 @@
-﻿namespace pms_entities
-{
-    public class Class1
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem.Entities.Configurations;
+using ProductManagementSystem.Entities.Models;
 
+namespace ProductManagementSystem.Entities
+{
+    public class RepositoryContext: DbContext
+    {
+        public RepositoryContext(DbContextOptions options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Address> Adresses { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<SupplierOffer> SupplierOffers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
+            modelBuilder.ApplyConfiguration(new BrandConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new SupplierConfiguration());
+            modelBuilder.ApplyConfiguration(new SupplierOfferConfiguration());
+        }
     }
 }
