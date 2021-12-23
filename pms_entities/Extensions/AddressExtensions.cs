52a65a75a430e2a167b655ef8976d4aa22b046a6
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 20:44:25 +0100

    Update AddressExtensions class

--- pms_entities/Extensions/AddressExtensions.cs
+++ pms_entities/Extensions/AddressExtensions.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ProductManagementSystem.Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pms_entities.Extensions
+namespace ProductManagementSystem.Entities.Extensions
 {
-    internal class AddressExtensions
+    public static class AddressExtensions
     {
+        public static void Map(this Address dbAddress, Address address)
+        {
+            dbAddress.Country = address.Country;
+            dbAddress.City = address.City;
+            dbAddress.Street = address.Street;
+            dbAddress.Number = address.Number;
+            dbAddress.ZipCode = address.ZipCode;
+        }
     }
 }
