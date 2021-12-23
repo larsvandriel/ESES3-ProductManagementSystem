9a747b7b6a1a0862f40bfd2937b93c048dde34c6
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 20:09:22 +0100

    Create BrandConfiguration.cs

---
+++ pms_entities/Configurations/BrandConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Entities.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.HasIndex(b => b.Name).IsUnique();
+            builder.Property(b => b.TimeCreated).IsRequired();
+            builder.Property(b => b.Deleted).IsRequired();
+            builder.Property(b => b.TimeDeleted);
+        }
+    }
+}
