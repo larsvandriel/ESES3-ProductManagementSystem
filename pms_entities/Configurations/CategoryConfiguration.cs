ea656d86b18a96f77606caeb51461b8de6e9d8f2
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 20:11:13 +0100

    Create CategoryConfiguration.cs

---
+++ pms_entities/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Entities.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Name).IsUnique();
+            builder.Property(c => c.Description);
+            builder.Property(c => c.TimeCreated).IsRequired();
+            builder.Property(c => c.Deleted).IsRequired();
+            builder.Property(c => c.TimeDeleted);
+        }
+    }
+}
