a49961381e8062da8f9bca3c62699a5ec8f37b5c
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 23:14:11 +0100

    Update QueryStringParameters

--- pms_entities/Parameters/QueryStringParameters.cs
+++ pms_entities/Parameters/QueryStringParameters.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace pms_entities.Parameters
+namespace ProductManagementSystem.Entities.Parameters
 {
-    internal class QueryStringParameters
+    public abstract class QueryStringParameters
     {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public string OrderBy { get; set; }
+        public string Fields { get; set; }
     }
 }
