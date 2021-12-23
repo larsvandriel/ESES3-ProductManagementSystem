88ce5e22d17bcb42a946f9f238a1a3a2704e1e18
larsvandriel <lars.van.driel@hotmail.com>
2021-12-23 20:44:33 +0100

    Create BrandExtensions.cs

---
+++ pms_entities/Extensions/BrandExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ProductManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.Entities.Extensions
+{
+    public static class BrandExtensions
+    {
+        public static void Map(this Brand dbBrand, Brand brand)
+        {
+            dbBrand.Name = brand.Name;
+            dbBrand.TimeCreated = brand.TimeCreated;
+            dbBrand.Deleted = brand.Deleted;
+            dbBrand.TimeDeleted = brand.TimeDeleted;
+        }
+    }
+}
