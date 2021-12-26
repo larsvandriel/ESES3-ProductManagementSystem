bc7f6319831e2903d4f67482dec113c0908d9a4e
larsvandriel <lars.van.driel@hotmail.com>
2021-12-26 01:52:31 +0100

    Setup API Settings ProductManagementSystem

--- pms_api/Program.cs
+++ pms_api/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using ProductManagementSystem.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
 
-builder.Services.AddControllers();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureMsSqlContext(configuration);
+builder.Services.ConfigureRepositoryWrapper();
+builder.Services.RegisterFilters();
+
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters().AddNewtonsoftJson();
+
+builder.Services.AddCustomMediaTypes();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +35,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
